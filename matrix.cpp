61b365c0c6f872e0093d02d6d83955a9a6d02b6b
dima <vasukenko9@gmail.com>
2025-07-05 04:27:09 +0300

    2 класса

--- matrix.cpp
+++ matrix.cpp
@@ -6,3 +6,24 @@
 using namespace Eigen;
 using namespace std;
 
+
+enum TYPE {R,P};
+
+enum OFF_AXIS {X,Y,Z};
+
+class Link {
+    
+    float length;
+    TYPE type;
+    OFF_AXIS axis;
+    
+};
+
+class Manipulator{
+        
+    };
+
+
+
+int main() {
+}
