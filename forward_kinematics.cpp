bb4199702c0d397b7679d6d355f8170d472b1d3d
dmitriyvasukov <vasukov0611@mail.ru>
2025-07-08 10:55:35 +0300

    разделил код на 2 файла, добавил расчет промежуточных матриц T

---
+++ forward_kinematics.cpp
@@ -0,0 +1,39 @@
+#include "geometry.hpp"
+
+int main() {
+
+    Matrix3f M0 = Matrix3f::Identity();
+
+    Matrix3f M1;
+    M1 << 0, 0, 1,
+          1, 0, 0,
+          0, 1, 0;
+    
+    Matrix3f M2;
+    M2 << 0, 1, 0,
+         -1, 0, 0,
+          0, 0, 1;
+    
+    Matrix3f M3;
+    M3 << 0, 0, 1,
+          1, 0, 0,
+          0, 1, 0;
+
+    
+    vector<Matrix3f>config{M0,M1,M2,M3};
+
+    link link1(0.8, Z, deg2rad(30.0), r);
+    link link2(0.6, X, deg2rad(20.0), r);
+    link link3(0.5, X, deg2rad(10.0), r);
+
+    vector<link>links{link1,link2,link3};
+
+    manipulator robot(links, config);
+
+    vector<Matrix4f> tm = robot.translation_matrix();
+
+    for(size_t i = 0; i < tm.size(); i++) {
+        cout << tm[i] << endl;
+        cout << endl;
+    }
+}
---
+++ geometry.hpp
@@ -0,0 +1,83 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace Eigen;
+using namespace std;
+
+float deg2rad(float degrees) {
+    return degrees * EIGEN_PI / 180.0f;
+}
+
+enum offset_axis {X,Y,Z};
+
+enum link_type {r = 1, p = 0};
+class link {
+
+    float  length;
+    public:
+    Vector3f offset_vector{0,0,0};
+    float angle;
+    float type;
+
+    link(float length, offset_axis axis, float angle,link_type type ): length(length), angle(angle), type(type)
+    {
+        offset_vector[axis] = length;
+    }
+};
+
+
+
+class manipulator {
+vector<link>links;
+
+
+vector<Matrix3f> initial_position;
+
+
+public:
+vector<Matrix4f> constant_geometry() {
+    vector<Matrix4f> constant_geometry;
+    constant_geometry.push_back(Matrix4f::Identity()); 
+
+    for (size_t i = 1; i < initial_position.size(); i++) {
+        Matrix4f A = Matrix4f::Identity();
+        A.block<3,3>(0,0) = initial_position[i];
+        A.block<3,1>(0,3) = links[i - 1].offset_vector; 
+        constant_geometry.push_back(A);
+    }
+
+    return constant_geometry;
+}
+
+vector<Matrix4f>translation_matrix() {
+    vector<Matrix4f> relative_matrices;
+    vector<Matrix4f> translation_matrices;
+    vector<Matrix4f> cg = constant_geometry();
+    Matrix4f final_matrix;
+    for(size_t i = 0; i < links.size(); i++ ) {
+        Matrix4f T;
+        T << 
+        cos(links[i].type * links[i].angle), -sin(links[i].type * links[i].angle), 0, 0,
+        sin(links[i].type * links[i].angle), cos(links[i].type * links[i].angle), 0, 0,
+        0,0,(1-links[i].type)*links[i].angle,0,
+        0,0,0,1;
+
+        relative_matrices.push_back(T);
+    }
+
+    for(size_t i = 0; i < cg.size()-1; i++) {
+        translation_matrices.push_back(cg[i] * relative_matrices[i]);
+    }
+
+    return translation_matrices;
+}
+
+    manipulator(vector<link>links,vector<Matrix3f> initial_position ) : links(links), initial_position(initial_position) {
+
+    }
+
+};
+
+ 
+
